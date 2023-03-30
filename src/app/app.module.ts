87870c43f6e23bc1b80a5f2b306f3189b1514af5
NerokArtworks <nerokprodscontact@gmail.com>
2023-03-30 18:49:19 +0200

    Home | Progreso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './views/index/index.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PoesiaComponent } from './components/sections/poesia/poesia.component';
+import { EBooksComponent } from './components/sections/e-books/e-books.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes:Routes = [
+  {path: 'index', component:IndexComponent},
+  {path: '**', redirectTo:"index"},
+  {path: ' ', redirectTo:"index"}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    NavbarComponent,
+    PoesiaComponent,
+    EBooksComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/sections/poesia/poesia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poesia',
+  templateUrl: './poesia.component.html',
+  styleUrls: ['./poesia.component.css']
+})
+export class PoesiaComponent {
+
+}
